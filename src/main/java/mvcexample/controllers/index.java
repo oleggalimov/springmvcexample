b44358146c5ea4b63b0ddaf6973ac139e0a12d80
oleg <o.d.galimov@yandex.ru>
2018-11-11 19:00:18 +0500

    Ready to deploy with heroku

--- src/main/java/mvcexample/controllers/index.java
+++ src/main/java/mvcexample/controllers/index.java
@@ -1,13 +1,29 @@
 package mvcexample.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.jndi.JndiTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import javax.naming.NamingException;
+
+//@Controller
+@RestController
 public class index {
+    @Autowired
+    private Environment env;
     @RequestMapping (value = "/index",  method = RequestMethod.GET)
     public String getIndexPage () {
-        return "index";
+        try {
+            String lookup = new JndiTemplate().lookup("jms/connectionFactory", String.class);
+            return lookup;
+        } catch (NamingException e) {
+            e.printStackTrace();
+            return e.getMessage();
+        }
+
     }
 }
